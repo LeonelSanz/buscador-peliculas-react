85f1d85ab76882c03a972025bc5a750410eab5ea
Leonel <leonelsanz15@gmail.com>
2023-08-22 14:52:02 -0300

    usar useRef para detectar cuando es la primera vez

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,19 @@ import { useMovies } from './hooks/useMovies';
 import { Movies } from './components/Movies';
 import { useState } from 'react';
 import { useEffect } from 'react';
+import { useRef } from 'react';
 
 function useSearch() {
   const [search, updateSearch] = useState('');
   const [error, setError] = useState(null);
+  const isFirstInput = useRef(true);
 
   useEffect(() => {
+    if (isFirstInput.current) {
+      isFirstInput.current = search === '';
+      return;
+    }
+
     if (search === '') {
       setError('No se puede buscar una pelicula vacia');
       return;
@@ -44,14 +51,16 @@ function App() {
     updateSearch(event.target.value);
   };
 
-  
-
   return (
     <div className='page'>
       <header>
         <h1>Buscador de peliculas</h1>
         <form className='form' onSubmit={handleSubmit}>
-          <input onChange={handleChange} value={search} name='query' placeholder='Avengers, Star Wars, The Matrix...' />
+          <input 
+            style={{
+              border: '1px solid transparent',
+              borderColor: error ? 'red' : 'transparent'
+            }} onChange={handleChange} value={search} name='query' placeholder='Avengers, Star Wars, The Matrix...' />
           <button type='submit'>Buscar</button>
         </form>
         {error && <p style={{ color: 'red' }}>{error}</p>}
