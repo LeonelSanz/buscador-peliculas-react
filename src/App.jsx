9cc5f6566ecd6782064d72646d95623eca615430
Leonel <leonelsanz15@gmail.com>
2023-08-22 14:43:23 -0300

    extraer useSearch a Custom Hook

--- src/App.jsx
+++ src/App.jsx
@@ -4,45 +4,54 @@ import { Movies } from './components/Movies';
 import { useState } from 'react';
 import { useEffect } from 'react';
 
-function App() {
-  const { movies } = useMovies();
-  const [query, setQuery] = useState('');
+function useSearch() {
+  const [search, updateSearch] = useState('');
   const [error, setError] = useState(null);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(query);
-  };
-
-  const handleChange = (event) => {
-    setQuery(event.target.value);
-  };
-
   useEffect(() => {
-    if (query === '') {
+    if (search === '') {
       setError('No se puede buscar una pelicula vacia');
       return;
     }
 
-    if (query.match(/^\d+$/)) {
+    if (search.match(/^\d+$/)) {
       setError('No se puede buscar una pelicula con un numero');
       return;
     }
 
-    if (query.length < 3) {
+    if (search.length < 3) {
       setError('No se puede buscar una pelicula menor a 3 caracteres');
       return;
     }
 
     setError(null);
-  }, [query]);
+  }, [search]);
+
+  return { search, updateSearch, error };
+
+}
+
+function App() {
+  const { movies } = useMovies();
+  const { search, updateSearch, error } = useSearch();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(search);
+  };
+
+  const handleChange = (event) => {
+    updateSearch(event.target.value);
+  };
+
+  
 
   return (
     <div className='page'>
       <header>
         <h1>Buscador de peliculas</h1>
         <form className='form' onSubmit={handleSubmit}>
-          <input onChange={handleChange} value={query} name='query' placeholder='Avengers, Star Wars, The Matrix...' />
+          <input onChange={handleChange} value={search} name='query' placeholder='Avengers, Star Wars, The Matrix...' />
           <button type='submit'>Buscar</button>
         </form>
         {error && <p style={{ color: 'red' }}>{error}</p>}
